5e78dc37d5d245ab38342a3f1da0d0eddadb3302
mukhutar <ngabomukhutar@gmail.com>
2024-01-29 16:29:46 +0200

    new commit

---
+++ gaara_form/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import FormOne from './components/formOne/formOne'
+
+function App() {
+  return (
+    <div>
+      <FormOne/>
+      
+    </div>
+  )
+}
+
+export default App
+
